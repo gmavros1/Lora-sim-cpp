eac472f30abb2232d17199a5fde6d91ac82684fe
gmavros1 <giorgosmauros2000@gmail.com>
2023-10-30 18:30:17 +0200

    receiving mechanism

--- src/gateway.cpp
+++ src/gateway.cpp
@@ -10,18 +10,28 @@
 void Gateway::receive(vector<radio_packet> &packets_received) {
 
     vector<radio_packet> current_packets = packets_received;
-    for (size_t index = 0; index < current_packets.size(); ++index) {
-        double receive_power = calculate_received_power(distanceGatewayNode(this->location, current_packets[index].location), current_packets[index].transmission_power);
 
+    // Abort packet due to range issues
+    for (int index = current_packets.size() - 1 ; index >= 0; index--) {
+        double receive_power = calculate_received_power(distanceGatewayNode(this->location,
+                                                                            current_packets[index].location),
+                                                        current_packets[index].transmission_power);
         if (receive_power >= -149){
-            cout << "KEPT" << endl;
             current_packets[index].receive_power = receive_power;
         } else{
-            cout << "ABORT" << endl;
             current_packets.erase(current_packets.begin() + index);
         }
-
     }
+
+    // Add a vector of structs. Its struct will contain:
+    //      * The id of the packet
+    //      * The sf
+    //      * The channel
+    //      * A vector of structs. which will contain
+    //            * The rec power
+    //            * The max power of the current segment packets in same channel and sf with different id
+
+
 
 }
 
