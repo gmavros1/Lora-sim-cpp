a281434e0f257e88ea23ab5f106622be3ab40ea6
gmavros1 <giorgosmauros2000@gmail.com>
2025-01-16 23:27:52 +0200

    fairness in node relation

---
+++ results/fairness_node.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
+pd.set_option('display.max_columns', None)
+
+results_df = pd.read_csv("metrics.txt")
+#display(results_df)
+
+# result_df = results_df.groupby(['case', 'rate'], as_index=False).mean()
+df = results_df[results_df['case'] == 'Multihop 1 gateways']
+
+df = df.groupby(['case', 'nodes_number'], as_index=False).mean()
+
+# df['fairness'] = (df['decoded'] / df['nodes_number']) / df['maximum_trans']
+
+# Create a line plot for each case
+cases = df['case'].unique()
+
+#plt.figure(figsize=(14, 8))
+
+for case in cases:
+    case_data = df[df['case'] == case]
+    plt.plot(case_data['nodes_number'], case_data['fairness'], label=f"Fairness - {case}")
+    #plt.plot(case_data['rate'], case_data['load'], label=f"Load - {case}", linestyle='--')
+
+plt.xlabel('Nodes Number')
+plt.ylabel('Jain\'s Fairness index')
+plt.legend()
+plt.title('Fairness vs Nodes\' number ')
+plt.legend()
+plt.grid(True)
+
+
+#plt.xlim(0, 1)
+#plt.ylim(0, 1)
+
+plt.show()
+
+
+
--- run_delay.sh
+++ run_delay.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "case,rate,decoded,non_decoded,nodes_number,life_time,maximum_trans,gen_packets,delay,max_delay,interference_in_node,out_of_range_in_ge,in_range_in_ge,max_sf,time_out,drop_p_async,fairness" > results/metrics.txt
-for r in {1..18}; do
+for r in {1..10}; do
   echo ""
-  nodes_num=$((80 * r))
+  nodes_num=$((20 * r))
   echo "Nodes number $nodes_num"
 
-  # Num nodes - If we want more range
-  python3 ./network_init/place_nodes.py "$nodes_num" range
-
   for i in {1..7}; do
+
+    # Num nodes - If we want more range
+    python3 ./network_init/place_nodes.py "$nodes_num" range
 
     rate=5
 
@@ -17,12 +17,12 @@ for r in {1..18}; do
 
     # Load - Time - Protocol - num of gateways - using adr in join process
 
-    python3 ./network_init/network_init.py "$rate" 100000 Multihop 1 "adr"
+    python3 ./network_init/network_init.py "$rate" 1000000 Multihop 1 "adr"
     cd cmake-build-debug || return
     ./Lora_sim_cpp
     cd ..
 
-    python3 ./network_init/network_init.py "$rate" 100000 LoRaWAn 1 "adr"
+    python3 ./network_init/network_init.py "$rate" 1000000 LoRaWAn 1 "adr"
     cd cmake-build-debug || return
     ./Lora_sim_cpp
     cd ..
@@ -32,4 +32,4 @@ done
 
 cd ./results || return
 # python3 metrics_th.py & python3 metrics_inf_gw.py
-python3 metrics_delay.py & python3 fairness_rate.py
+python3 metrics_delay.py & python3 fairness_node.py
